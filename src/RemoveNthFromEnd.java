c2a4dcd73b19d02be9aadc6c7d0c27815b02d6db
feng.hou <houyaerda@gmail.com>
2020-06-11 00:44:11 +0800

    fix

--- src/RemoveNthFromEnd.java
+++ src/RemoveNthFromEnd.java
@@ -1,47 +1,46 @@
 public class RemoveNthFromEnd {
 
     public static void main(String[] args) {
-        LinkNode head = new LinkNode(6);
-
-        for (int i = 1; i <= 5; i++) {
-            LinkNode next = head.next;
-            head.next = new LinkNode(i);
-            head.next.next = next;
-        }
+        ListNode head = new ListNode(1);
+//
+//        for (int i = 1; i <= 1; i++) {
+//            ListNode next = head.next;
+//            head.next = new ListNode(i);
+//            head.next.next = next;
+//        }
         System.out.println(head);
-        System.out.println(removeNth(head, 3));
+        System.out.println(removeNth(head, 1));
     }
 
-    private static LinkNode removeNth(LinkNode head, int n) {
-        LinkNode start = new LinkNode(0);
-        LinkNode slow = start, fast = start;
-        start.next = head;
-        for (int i = 1; i <= n+1; i++) {
+    private static ListNode removeNth(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+        for (int i = 0; i < n + 1; i++) {
             fast = fast.next;
         }
-        while (fast!= null) {
+        while (fast != null) {
             fast = fast.next;
             slow = slow.next;
         }
-        //remove not desired element
         slow.next = slow.next.next;
-        return head;
+        return start.next;
     }
 }
 
 
-class LinkNode {
+class ListNode {
     int val;
-    LinkNode next;
+    ListNode next;
 
-    LinkNode() {
+    ListNode() {
     }
 
-    LinkNode(int val) {
+    ListNode(int val) {
         this.val = val;
     }
 
-    LinkNode(int val, LinkNode node) {
+    ListNode(int val, ListNode node) {
         this.next = node;
         this.val = val;
     }
